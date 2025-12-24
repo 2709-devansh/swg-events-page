6ffb24cb6bceab037112b81587e6cb8d6b126e98
2709-devansh <devaanshpaanthi@gmail.com>
2025-12-25 00:28:43 +0530

    third time's the charm(let's hope)

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -11,7 +11,7 @@ const Events = () => {
       category: 'welfare',
       title: 'Student Mentorship Program',
       description: 'An important initiative to connect seniors with freshers and mentor them throughout their first year journey, and further as a guide and support.',
-      image: {SMP},
+      image: SMP,
     },
     {
       id: 2,
